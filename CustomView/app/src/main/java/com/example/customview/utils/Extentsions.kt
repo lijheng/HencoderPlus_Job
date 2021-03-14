a25693a467dcb590365db71f2104091c675a54df
summer <lijheng@qq.com>
2021-03-14 16:46:25 +0800

    有点问题的camera

--- CustomView/app/src/main/java/com/example/customview/utils/Extentsions.kt
+++ CustomView/app/src/main/java/com/example/customview/utils/Extentsions.kt
@@ -1,11 +1,12 @@
 package com.example.customview.utils
 
 import android.content.res.Resources
+import android.util.TypedValue
 
 
 val displayMetrics = Resources.getSystem().displayMetrics
 val Float.dp: Float
-    get() = displayMetrics.density * this + 0.5f
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics)
 
 val Int.dp: Float
-    get() = displayMetrics.density * this + 0.5f
+        get() = this.toFloat().dp
--- CustomView/app/src/main/java/com/example/customview/widget/CameraView.kt
+++ CustomView/app/src/main/java/com/example/customview/widget/CameraView.kt
@@ -6,6 +6,7 @@ import android.util.AttributeSet
 import android.view.View
 import com.example.customview.R
 import com.example.customview.utils.dp
+import kotlin.math.sqrt
 
 class CameraView(context: Context, attr: AttributeSet) : View(context, attr) {
 
@@ -16,31 +17,51 @@ class CameraView(context: Context, attr: AttributeSet) : View(context, attr) {
 
     private val mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private val mBitmap = getImage(IMAGE_SIZE)
+    private val mBitmap = getImage(IMAGE_SIZE.toInt())
 
     private val mCamera = Camera().apply {
         rotateX(30f)
+        translate(0f ,0f, -6 * resources.displayMetrics.density)
     }
 
 
+    //绕X轴翻转总体思路：
+    //1.需要将画布的中心移动到Camera的中心点，才能保证投影比例正常
+    //2.投影（即翻转）
+    //3.将画布移动回去
+    //4.在部分翻转时使用几何裁剪来裁剪画布
+    //5.
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        val count = canvas.save()
-        canvas.translate(-(IMAGE_PADDING + IMAGE_SIZE/2),-(IMAGE_PADDING+ IMAGE_SIZE/2))
+//        super.onDraw(canvas)
+        //上半部分不围绕X轴翻起
+        canvas.save()
+        canvas.translate((IMAGE_PADDING + IMAGE_SIZE / 2), (IMAGE_PADDING + IMAGE_SIZE / 2))
+        canvas.rotate(-30f)
+        canvas.clipRect(-IMAGE_SIZE, -IMAGE_SIZE, IMAGE_SIZE , 0f)
+        canvas.rotate(30f)
+        canvas.translate(-(IMAGE_PADDING + IMAGE_SIZE / 2), -(IMAGE_PADDING + IMAGE_SIZE / 2))
         canvas.drawBitmap(mBitmap, IMAGE_PADDING, IMAGE_PADDING, mPaint)
+        canvas.restore()
+
+        //下半部分围绕X轴翻转
+        canvas.save()
+        canvas.translate((IMAGE_PADDING + IMAGE_SIZE / 2), (IMAGE_PADDING + IMAGE_SIZE / 2))
+        canvas.rotate(-30f)
         mCamera.applyToCanvas(canvas)
-        canvas.translate((IMAGE_PADDING + IMAGE_SIZE/2),(IMAGE_PADDING+ IMAGE_SIZE/2))
-        canvas.restoreToCount(count)
+        canvas.clipRect(-IMAGE_SIZE, 0f, IMAGE_SIZE,
+            IMAGE_SIZE )
+        canvas.rotate(30f)
+        canvas.translate(-(IMAGE_PADDING + IMAGE_SIZE / 2), -(IMAGE_PADDING + IMAGE_SIZE / 2))
+        canvas.drawBitmap(mBitmap, IMAGE_PADDING, IMAGE_PADDING, mPaint)
+        canvas.restore()
     }
 
-
-
-    private fun getImage(width: Float): Bitmap {
+    private fun getImage(width: Int): Bitmap {
         val options = BitmapFactory.Options()
         options.inJustDecodeBounds = true
         BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
         options.inDensity = options.outWidth
-        options.inTargetDensity = width.toInt()
+        options.inTargetDensity = width
         options.inJustDecodeBounds = false
         return BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
     }
